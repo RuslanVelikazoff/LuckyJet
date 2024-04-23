3f97b08bc8d8d7674c63ef063839e524fc5d7fda
RuslanVelikazoff <123361452+RuslanVelikazoff@users.noreply.github.com>
2024-04-23 11:02:31 +0300

    CompletedSettings

---
+++ Assets/Scripts/ControlManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class ControlManager : MonoBehaviour
+{
+    private const string PLAYER_PREFS_CONTROL = "Control";
+    private const string PLAYER_PREFS_EXPERT = "Expert";
+    private const string PLAYER_PREFS_CLASSIC = "Classic";
+    
+    public static ControlManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        if (!PlayerPrefs.HasKey(PLAYER_PREFS_CONTROL))
+        {
+            SetControlClassic();
+        }
+    }
+
+    public void SetControlExpert()
+    {
+        PlayerPrefs.SetString(PLAYER_PREFS_CONTROL, PLAYER_PREFS_EXPERT);
+    }
+
+    public void SetControlClassic()
+    {
+        PlayerPrefs.SetString(PLAYER_PREFS_CONTROL, PLAYER_PREFS_CLASSIC);
+    }
+
+    public bool IsClassicControl()
+    {
+        if (PlayerPrefs.GetString(PLAYER_PREFS_CONTROL) == PLAYER_PREFS_CLASSIC)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- Assets/Scripts/MainMenu/ControlPanel.cs
+++ Assets/Scripts/MainMenu/ControlPanel.cs
@@ -1,16 +1,86 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlPanel : MonoBehaviour, IPanelFunction
 {
+    [SerializeField]
+    private Button classicButton;
+    [SerializeField] 
+    private Image selectedClassicImage;
+
+    [SerializeField] 
+    private Button expertButton;
+    [SerializeField] 
+    private Image selectedExpertImage;
+
+    [SerializeField]
+    private Button backButton;
+
+    [SerializeField] 
+    private GameObject settingsPanel;
+
+    [SerializeField] 
+    private Sprite selectedSprite;
+    [SerializeField] 
+    private Sprite unSelectedSprite;
+    
     private IPanelFunction panelFunction;
 
     private void Awake()
     {
-        
+        panelFunction = this;
+        ButtonClickAction();
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        SetButtonsSprite();
     }
 
     public void ButtonClickAction()
     {
-        throw new System.NotImplementedException();
+        if (backButton != null)
+        {
+            backButton.onClick.RemoveAllListeners();
+            backButton.onClick.AddListener(() =>
+            {
+                panelFunction.OpenPanel(settingsPanel, this.gameObject);
+            });
+        }
+
+        if (classicButton != null)
+        {
+            classicButton.onClick.RemoveAllListeners();
+            classicButton.onClick.AddListener(() =>
+            {
+                ControlManager.Instance.SetControlClassic();
+                SetButtonsSprite();
+            });
+        }
+
+        if (expertButton != null)
+        {
+            expertButton.onClick.RemoveAllListeners();
+            expertButton.onClick.AddListener(() =>
+            {
+                ControlManager.Instance.SetControlExpert();
+                SetButtonsSprite();
+            });
+        }
+    }
+
+    private void SetButtonsSprite()
+    {
+        if (ControlManager.Instance.IsClassicControl())
+        {
+            selectedClassicImage.sprite = selectedSprite;
+            selectedExpertImage.sprite = unSelectedSprite;
+        }
+        else
+        {
+            selectedClassicImage.sprite = unSelectedSprite;
+            selectedExpertImage.sprite = selectedSprite;
+        }
     }
 }
